e34b88e9c3a2e5247f8e936e9d55dfa41aa16e53
Tim McElwee <tim@Tims-MacBook-Pro.local>
2022-06-08 13:05:10 -0400

    updated values to better check for zero game days, particularly in the playoffs

--- js/score-base.js
+++ js/score-base.js
@@ -4,6 +4,7 @@ export class ScoreBase extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.totalGames = 0;
     this.games = {};
     this.copyright = '';
   }
@@ -11,8 +12,9 @@ export class ScoreBase extends HTMLElement {
   async connectedCallback() {
     const response = await fetch('https://statsapi.web.nhl.com/api/v1/schedule');
     const json = await response.json();
-    this.games = await json.dates[0];
     this.copyright = await json.copyright;
+    this.games = await json.dates[0];
+    this.totalGames = await json.totalGames;
     this.render();
   }
 
@@ -23,7 +25,7 @@ export class ScoreBase extends HTMLElement {
   render() {
     if (this.loading) {
       this.shadowRoot.innerHTML = `Loading...`;
-    } else {
+    } else if (this.totalGames > 0) {
       console.log('render: this.games = ', this.games);
       this.shadowRoot.innerHTML = `
       <style>
@@ -46,13 +48,18 @@ export class ScoreBase extends HTMLElement {
           const homeRecord = game.teams.home.leagueRecord
           return `
             <li>
-              <game-block feed="${game.link}" away-record="(${awayRecord.wins}-${awayRecord.losses}-${awayRecord.ot})" home-record="(${homeRecord.wins}-${homeRecord.losses}-${homeRecord.ot})"></game-block>
+              <game-block feed="${game.link}" away-record="(${awayRecord.wins !== undefined ? awayRecord.wins : ''}-${awayRecord.losses !== undefined ? awayRecord.losses : ''}${awayRecord.ot ? - awayRecord.ot : ''})" home-record="(${homeRecord.wins !== undefined ? homeRecord.wins : ''}-${homeRecord.losses !== undefined ? homeRecord.losses : ''}${homeRecord.ot !== undefined ? - homeRecord.ot : ''})"></game-block>
             </li>
           `;
         }).join("")}
       </ul>
 
       <p>Disclaimer: <small>${this.copyright}</small></p>`
+    } else {
+      this.shadowRoot.innerHTML = `
+        <p>There a no games available for today.</p>
+        <p>Disclaimer: <small>${this.copyright}</small></p>
+      `
     }
   }
 }
